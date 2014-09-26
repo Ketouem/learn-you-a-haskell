26ba97de3e468044604289bf5b60dabd1ce1d8d6
Ketouem <ketouem@gmail.com>
2014-09-26 15:32:12 +0200

    Up to the Data.Map module.

--- src/chapter-6-modules/modules.hs
+++ src/chapter-6-modules/modules.hs
@@ -88,3 +88,64 @@ decode shift msg = encode (negate shift) msg
 -- Data.list.foldl' provides a stricter version of foldl where the accumulator
 -- is evalutaed at each step.
 -- A stricter version of foldl1 exists -> foldl1'
+
+-- Finding some cool numbers
+
+-- Ex: find the first natural number such that the sum of its digits equals 40
+
+-- Data.Char.digitToInt : takes a Char and return an Int
+--                        '0' to '9' and 'A' to 'F'
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+-- `show` is use to convert the integer into a string
+
+-- Data.List.find : takes a predicate and a list and returns the first element
+--                  in the list that matches the predicate.
+--                  find :: (a -> Bool) -> [a] -> Maybe a
+--                  Maybe a ~ [a] . Whereas a list can have zero, one, or many
+--                  elements, a Maybe a typed value can have either 0 elements
+--                  or juste one elements. We use it when we want to represent
+--                  possible failure. To make a value that holds nothing, we
+--                  just use `Nothing`. Analogous to an empty list. To construct
+--                  a value that holds something, we write `Just <value .
+--                  If `find` finds an element that satisfies the predicate, it
+--                  will return that element wrapped in a `Just`, else it will
+--                  return a `Nothing`
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]
+
+-- Mapping keys to values
+
+-- Association lists (aka dictionaries)
+
+-- Most obvious way to represent it in Haskell would be a list of pairs.
+-- The first value being the key and the second the value.
+
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")]
+
+-- Lookup for a value given a key
+findKey :: (Eq k) => k -> [(k, v)] -> v
+findKey key xs = snd . head . filter (\(k, v) -> key == k) $ xs
+-- The function takes a key and a list, filters the list so that only matching
+-- keys remain, gets the first key/value pair that matches and returns the value
+
+-- Handling missing key
+findKey' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey' key [] = Nothing
+findKey' key ((k,v):xs)
+    | key == k  = Just v
+    | otherwise = findKey' key xs
+
+-- folded version
+findKey'' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey'' key xs = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing xs
+
+-- Enter Data.Map
