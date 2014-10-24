1cbd1f5437a8d77aac3b888bdba8f7e3e0305bed
Ketouem <ketouem@gmail.com>
2014-10-24 18:00:01 +0200

    Up to the module creation

---
+++ src/chapter-6-modules/geometry.hs
@@ -0,0 +1,7 @@
+-- Making our own modules
+
+-- When we import a module, we can use the functions that it exports. A module
+-- can also defines functions that it uses internally, but we can see and only
+-- use only the ones that it exports
+
+-- A geometry modules
